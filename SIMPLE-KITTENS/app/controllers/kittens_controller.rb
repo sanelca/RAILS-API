6c396916f02e8c192f124613bb1c02f308d76f3e
Sanel Hodzic <sanel_hodzic_tz_91@hotmail.com>
2019-06-21 11:18:25 +0200

    Simple kittens basic comands

--- SIMPLE-KITTENS/app/controllers/kittens_controller.rb
+++ SIMPLE-KITTENS/app/controllers/kittens_controller.rb
@@ -8,4 +8,44 @@ class KittensController < ApplicationController
       format.json { render :json => @kittens}
     end
   end
+
+  def show
+    @kitten = Kitten.find(params[:id])
+  end
+  
+  def new
+    @kitten = Kitten.new
+  end
+
+  def edit
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+    respond_to do |format|
+      if @kitten.save
+        flash[:notice] = 'Kitten was successfully created.'
+        format.html { redirect_to @kitten, notice: 'Kitten was successfully created.' }
+        format.json { render :show, status: :created, location: @kitten }
+      else
+        format.html { render :new }
+        format.json { render json: @kitten.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    @kitten = Kitten.find(params[:id])
+    respond_to do |format|
+      if @kitten.update(kitten_params)
+        flash[:notice] = 'Kitten was successfully updated.'
+        format.html { redirect_to @kitten, notice: 'Kitten was successfully updated.' }
+        format.json { render :show, status: :ok, location: @kitten }
+      else
+        format.html { render :edit }
+        format.json { render json: @kitten.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 end
