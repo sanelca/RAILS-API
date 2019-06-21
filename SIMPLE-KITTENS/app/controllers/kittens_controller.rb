925aa70b15cacffce95706e68c8ae387fff3f2f1
Ifeanyi Osinakayah Joseph <ifeanyi.Osinakayah@cousant.com>
2019-06-21 09:55:10 +0100

    Designed the kitten module

--- SIMPLE-KITTENS/app/controllers/kittens_controller.rb
+++ SIMPLE-KITTENS/app/controllers/kittens_controller.rb
@@ -1,2 +1,11 @@
 class KittensController < ApplicationController
+  include KittensHelper
+  def index
+    @kittens = Kitten.all
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml { render :xml => @kittens }
+      format.json { render :json => @kittens}
+    end
+  end
 end
